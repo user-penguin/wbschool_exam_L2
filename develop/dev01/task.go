7dfe052acb746763334fcd9f47b2f22adccc814e
user-penguin <kobzev96@gmail.com>
2022-04-12 23:53:13 +0700

    try to get time with ntp package

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"time"
+)
+
 /*
 === Базовая задача ===
 
@@ -11,7 +17,18 @@ package main
 Программа должна корректно обрабатывать ошибки библиотеки: распечатывать их в STDERR и возвращать ненулевой код выхода в OS.
 Программа должна проходить проверки go vet и golint.
 */
-
 func main() {
+	ntpTime, err := ntp.Time("time.nist.gov")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	ntpTimeFormatted := ntpTime.Format(time.UnixDate)
 
+	fmt.Printf("Network time: %v\n", ntpTime)
+	fmt.Printf("Unix Date Network time: %v\n", ntpTimeFormatted)
+	fmt.Println("+++++++++++++++++++++++++++++++")
+	timeFormatted := time.Now().Local().Format(time.UnixDate)
+	fmt.Printf("System time: %v\n", time.Now())
+	fmt.Printf("Unix Date System time: %v\n", timeFormatted)
 }
